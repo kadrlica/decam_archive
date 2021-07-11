22b0a9d7bcc0209c3fea87d0e2c7071d3a2d7bc4
Alex Drlica-Wagner <kadrlica@fnal.gov>
2021-07-11 16:47:02 -0500

    more options for archive fill

--- bin/run_fill_archive.sh
+++ bin/run_fill_archive.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+STOPDATE=`date +%Y%m%d`
+if [ $# == 1 ] ; then  STOPDATE=$1 ; fi
+
 umask 002
 
 date
@@ -23,7 +26,12 @@ export PATH=/home/s1/kadrlica/bin:$PATH # for csub
 #prune_archive --outdir=$SRCDIR
 
 echo "Filling archive..."
-fill_archive -v --njobs 5 --outdir=$OUTDIR
+# Fill the entire archive
+fill_archive -v --njobs 4 --outdir=$OUTDIR --tstop $STOPDATE --exptime 30 --logdir log/download
+
+# Just a single propid
+#PROPID=2020B-0282 #2019A-0305 #2020B-0282 #2020A-0399 #2020B-0241
+#fill_archive -v --njobs 5 --outdir=$OUTDIR --propid $PROPID
 
 ## The `-n 1` argument delays subsequent jobs until fill_archive is done
  
