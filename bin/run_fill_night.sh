9c128308fc4ba5d1b4c105928409270af3343c40
Alex Drlica-Wagner <kadrlica@fnal.gov>
2021-07-11 16:41:49 -0500

    don'tpull from ncsa

--- bin/run_fill_night.sh
+++ bin/run_fill_night.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "-h" ] ; then echo "Usage: fill_nightly.sh <YYYYMMDD>" ; exit 1 ; fi
-if [ "$1" == "-help" ] ; then echo "Usage: fill_nightly.sh <YYYYMMDD>" ; exit 1 ; fi
-if [ "$1" == "--help" ] ; then echo "Usage: fill_nightly.sh <YYYYMMDD>" ; exit 1 ; fi
+if [ "$1" == "-h" ]     ; then echo "Usage: run_fill_night.sh <YYYYMMDD>" ; exit 1 ; fi
+if [ "$1" == "-help" ]  ; then echo "Usage: run_fill_night.sh <YYYYMMDD>" ; exit 1 ; fi
+if [ "$1" == "--help" ] ; then echo "Usage: run_fill_night.sh <YYYYMMDD>" ; exit 1 ; fi
 
 STARTDATE=`date --date="1 day ago"  +%Y%m%d`
 if [ $# == 1 ] ; then  STARTDATE=$1 ; fi
@@ -21,7 +21,7 @@ cd $OUTDIR
 #wget -X . -r -A DECam_\*fits.fz  -np --level=2 --no-check-certificate -N -nH --cut-dirs=4 --progress=dot -e dotbytes=4M $url/${STARTDATE}/
 
 url=http://decade.ncsa.illinois.edu/deca_archive/RAW
-wget -X . -r -A DECam_\*fits.fz  -np --level=2 --no-check-certificate -N -nH --cut-dirs=2 --progress=dot -e dotbytes=4M $url/${STARTDATE}/
+#wget -X . -r -A DECam_\*fits.fz  -np --level=2 --no-check-certificate -N -nH --cut-dirs=2 --progress=dot -e dotbytes=4M $url/${STARTDATE}/
 
 # Setup conda
 export PATH=/cvmfs/des.opensciencegrid.org/fnal/anaconda2/envs/default/bin:$PATH
@@ -33,12 +33,12 @@ export PYTHONPATH=$DECAM_ARCHIVE:$PYTHONPATH
 export PATH=$DECAM_ARCHIVE/bin:$PATH
 export PATH=/home/s1/kadrlica/bin:$PATH # for csub
 
-# Try moving back an additional day
-STARTDATE=`date --date="$STARTDATE - 2 day"  +%Y%m%d`
 # Default with no propid specified
-fill_night --njobs 5 --date=$STARTDATE --outdir=$OUTDIR -v \
-#     --propid="2019A-0272"
-# To target one or more propids, uncomment the line above
+fill_night --njobs 5 --date=$STARTDATE --outdir=$OUTDIR -v 
+
+# To target one or more propids:
+#fill_night --njobs 5 --outdir=$OUTDIR -v --propid="2020B-0282"
+    
 
 echo "Linking nite to $SRCDIR..."
 link_archive --njobs 5 --indir $OUTDIR --outdir $SRCDIR
